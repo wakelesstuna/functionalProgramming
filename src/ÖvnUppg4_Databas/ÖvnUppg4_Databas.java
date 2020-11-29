1179802ed1623e18af03d11facc4920438573d46
Sigrun Olafsdottir <solafsdottir@gmail.com>
2020-11-29 17:31:46 +0100

    Text i ÖvnUppg4 uppdaterad

--- src/ÖvnUppg4_Databas/ÖvnUppg4_Databas.java
+++ src/ÖvnUppg4_Databas/ÖvnUppg4_Databas.java
@@ -5,56 +5,86 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/*
+Inlämningsuppgit i kursen Funktionell Programmering för JAVA-programmet
+
+För samtliga funktioner i denna fil, byt ut "throw UnSupportedException"-raden
+och skriv ett pipeline-uttryck som returnerar det som ska returneras.
+
+Streams MÅSTE användas i samtliga funktioner som lämnas in
+För att testa om era funktioner funkar, kör testerna som hör till denna fil
+
+För G på denna uppgift, implementera alla funktioner märkta med G
+FÖr VG, implementera alla funktioner märkta med G OCH alla funktioner märkta med VG
+
+Sigruns bedömning av denna uppgift kommer att gå till så att hon klipper in er version av denna fil
+i sitt projekt och kör testerna.
+Hennes kontroll om ni har klarat uppgifterna eller inte görs genom att
+hon kollar att tillräckeligt många av tester går gröna. Pga detta ska ni inte ändra i någon fil
+medföljande detta projekt, och inte heller metodsignaturerna i denna fil eller era tester, eftersom
+ni då riskerar att påverka rättningen i negativ riktning.
+ */
 
 public class ÖvnUppg4_Databas {
     
     public Database database = new Database();
     public List<Question> questions = database.getQuestions();
     
-    //G. Hur många frågor finns i databasen?
+    //G. Skriv en funktioner som returnerar hur många frågor det finns i databasen?
     public int getAmountOfQuestionsInDatabase(){
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
     
-    //G. Hitta antalet frågor för en viss, given kategori
+    //G. Hur många frågor finns i databasen för en viss, given kategori (som ges som inparameter)
     public int getAmountOfQuestionsForACertainCategory(Category category){
        throw new UnsupportedOperationException("Not supported yet.");
+
     }
 
-    //G. Lista alla frågesträngar i databasen
+    //G. Skapa en lista innehållandes samtliga frågesträngar i databasen
     public List<String> getListOfAllQuestions(){
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
     
-    //G. Lista alla frågesträngar där frågan tillhör en viss kategori
+    //G. Skapa en lista innehållandes samtliga frågesträngar där frågan tillhör en viss kategori
+    //Kategorin ges som inparameter
     public List<String> getAllQuestionStringsBelongingACategory(Category category){
         throw new UnsupportedOperationException("Not supported yet.");
+
     }
 
-    //G. Skapa en lista av alla svarsalternativ där varje svarsalternativ får förekomma en och endast en gång
+    //G. Skapa en lista av alla svarsalternativ, där varje svarsalternativ får förekomma
+    // en och endast en gång i den lista som du ska returnera
     public List<String> getAllAnswerOptionsDistinct(){
         throw new UnsupportedOperationException("Not supported yet.");
+
     }
 
 
-    //VG. Finns en viss given sträng som svarsalternativ till någon fråga i vår databas?
+    //VG. Finns en viss sträng, given som inparameter, som svarsalternativ till någon fråga i vår databas?
     public boolean isThisAnAnswerOption(String answerCandidate){
         throw new UnsupportedOperationException("Not supported yet.");
+
     }
 
-    //VG. Hur ofta förekommer ett visst svarsalternativ i databasen
+    //VG. Hur ofta förekommer ett visst svarsalternativ, givet som inparameter, i databasen
     public int getAnswerCandidateFrequncy(String answerCandidate){
+
         throw new UnsupportedOperationException("Not supported yet.");
+
     }
 
     //VG. Skapa en Map där kategorierna är nycklar och värdena är en lista
     //av de frågesträngar som tillhör varje kategori
     public Map<Category, List<String>> getQuestionGroupedByCategory(){
+
        throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    //VG. Skapa en funktion som hittar det svarsalternativ som har felst bokstäver, i en kategori
-    // Använd Reduce!
+    //VG. Skapa en funktion som hittar det svarsalternativ som har flest bokstäver, i en kategori, given som inparameter
+    // Du måste använda Reduce!
     public String getLongestLettercountAnwerInAGivenCategory(Category c)
     {
         throw new UnsupportedOperationException("Not supported yet.");
