cec5508577fb9918af1115a4609b3e8a72fe57ff
Oscar Forss <oscar.forss.1@gmail.com>
2021-01-13 20:12:41 +0100

    made makemap() -> 1 line

--- src/InlämningsUppgiftVG/santaClausHierarchyKotlin.kt
+++ src/InlämningsUppgiftVG/santaClausHierarchyKotlin.kt
@@ -4,29 +4,19 @@ import java.io.File
 
 fun makeStringList (fileName: String) = File(fileName).readLines()
 
-fun makeMap(): MutableMap<String, MutableList<String>> {
-    val listOfInput = makeStringList("src/InlämningsUppgiftVG/santaClausHierarchy.txt")
+val listOfInput = makeStringList("src/InlämningsUppgiftVG/santaClausHierarchy.txt")
 
-    val mapperino: MutableMap<String, MutableList<String>> = mutableMapOf()
-
-    for (i in listOfInput){
-        var splitLine = i.split(" ")
-        val key = splitLine[0]
-        splitLine = splitLine.drop(1)
-        mapperino[key] = splitLine as MutableList<String>
-    }
-    return mapperino
-}
+fun makeMap(): Map<String, List<String>> = listOfInput.map { e -> e.split(" ") }.associate { e -> e[0] to e.drop(1) }
 
 val mapperino = makeMap()
 
-fun addSubBossToList(name: String): List<String> {
+fun addSubBosses(name: String): List<String> {
     val temp: MutableList<String> = mutableListOf()
     return when {
         mapperino.containsKey(name) -> {
             mapperino.getValue(name)
                     .toCollection(mutableListOf())
-                    .forEach { e -> temp.add(e); addSubBossToList(e).forEach{ e -> temp.add(e) };  }
+                    .forEach { e -> temp.add(e); addSubBosses(e).forEach{ v -> temp.add(v) };  }
             temp
         }
         else -> temp
@@ -34,6 +24,6 @@ fun addSubBossToList(name: String): List<String> {
 }
 
 fun main() {
-    val temp = addSubBossToList("Tomten")
+    val temp = addSubBosses("Tomten")
     temp.forEach { s -> println(s) }
 }
