87953be80c80895f1bbdac1dde51dbf8e58277f0
Oscar Forss <oscar.forss.1@gmail.com>
2021-01-16 11:35:29 +0100

    clean up, Done!

--- src/InlämningsUppgiftVG/SantaClausWorkShop.java
+++ src/InlämningsUppgiftVG/SantaClausWorkShop.java
@@ -8,17 +8,17 @@ import java.util.*;
 
 public class SantaClausWorkShop {
 
-    protected List<String> input = readFileString("src/InlämningsUppgiftVG/santaClausHierarchy.txt");
+    protected List<String> inputs = readFileString("src/InlämningsUppgiftVG/santaClausHierarchy.txt");
 
     protected final ImageIcon santaClaus = new ImageIcon("src/InlämningsUppgiftVG/Angry-SantaClaus.png");
     protected final ImageIcon mrsSanta = new ImageIcon("src/InlämningsUppgiftVG/MrsClaus.jpg");
 
-    protected List<String> bosses = new ArrayList<>();
-    protected List<String> subBosses = new ArrayList<>();
     protected List<String> result = new ArrayList<>();
 
     protected String inputName = "";
     protected Boolean running = true;
+    protected StringBuilder sb = new StringBuilder();
+    protected String input = "";
 
     public static void main(String[] args) {
         new SantaClausWorkShop();
@@ -34,86 +34,85 @@ public class SantaClausWorkShop {
                 case BOSSES -> getBosses();
                 case SUBBOSSES -> getSubBosses();
             }
+            int i = JOptionPane.showConfirmDialog(null, "Do you wanna search for more?");
+            if (!(i == 0)) {
+                running = false;
+            }
         }
-
     }
 
     public void getBosses() {
-        String name = JOptionPane.showInputDialog(null,
+        input = JOptionPane.showInputDialog(null,
                 "Input the name of the santa, to get the bosses: ",
                 "Bosses", JOptionPane.INFORMATION_MESSAGE);
 
-        inputName = name.substring(0, 1).toUpperCase() + name.substring(1);
-        result = findBosses(name);
+        inputName = input.substring(0, 1).toUpperCase() + input.substring(1);
+        result = findBosses(input);
 
         if (result.size() == 0) {
             getMrsSanta();
         } else {
-            StringBuilder sb = new StringBuilder();
             result.forEach(s -> sb.append(s).append("\n"));
             JOptionPane.showMessageDialog(null,
                     "Bosses: \n" + sb.toString(),
                     "Sub-boss: " + inputName, JOptionPane.INFORMATION_MESSAGE);
-            running = false;
         }
 
     }
 
     // recursive method
     public List<String> findBosses(String name) {
-        for (String lines : input) {
+        List<String> temp = new ArrayList<>();
+        for (String lines : inputs) {
             String[] lineSplit = lines.split(" ");
             String bossName = lineSplit[0];
             String allSubBosses = lines.substring(bossName.length() + 1);
             String[] subBoss = allSubBosses.split(" ");
             for (String s : subBoss) {
                 if (s.equalsIgnoreCase(name)) {
-                    bosses.add(0, bossName);
-                    findBosses(bossName);
+                    temp.add(0, bossName);
+                    temp.addAll(0, findBosses(bossName));
                 }
             }
         }
-        return bosses;
+        return temp;
     }
 
     public void getSubBosses() {
-        String name = JOptionPane.showInputDialog(null,
+        input = JOptionPane.showInputDialog(null,
                 "Input the name of the santa, to get the sub-bosses: ",
                 "Sub-bosses", JOptionPane.INFORMATION_MESSAGE);
 
-        inputName = name.substring(0, 1).toUpperCase() + name.substring(1);
-        result = findSubBosses(name);
-
+        inputName = input.substring(0, 1).toUpperCase() + input.substring(1);
+        result = findSubBosses(input);
 
         if (result.size() == 0) {
             JOptionPane.showMessageDialog(null,
                     inputName + " has no sub-bosses, he is at the bottom of the pack!");
         } else {
-            StringBuilder sb = new StringBuilder();
             result.forEach(s -> sb.append(s).append("\n"));
             JOptionPane.showMessageDialog(null,
                     "Sub-bosses: \n" + sb.toString(),
                     "Boss: " + inputName, JOptionPane.INFORMATION_MESSAGE);
-
-            running = false;
         }
     }
 
     // recursive method
     public List<String> findSubBosses(String name) {
-        for (String lines : input) {
+        List<String> temp = new ArrayList<>();
+        for (String lines : inputs) {
             String[] lineSplit = lines.split(" ");
             String bossName = lineSplit[0];
             String allSubBosses = lines.substring(bossName.length() + 1);
             String[] subBoss = allSubBosses.split(" ");
             if (name.equalsIgnoreCase(bossName)) {
                 for (String s : subBoss) {
-                    subBosses.add(s);
-                    findSubBosses(s);
+                    temp.add(s);
+                    temp.addAll(findSubBosses(s));
                 }
             }
         }
-        return subBosses;
+        return temp;
     }
 
     public void getMrsSanta() {
